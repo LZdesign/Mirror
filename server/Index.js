2603d5fc34cf33ae354cbd94a9e916b1a01cf14e
LZdesign <lzottoli93@gmail.com>
2023-03-26 10:06:22 +0200

    Added a server folder

---
+++ server/Index.js
@@ -0,0 +1,104 @@
+import 'dotenv/config';
+import { Configuration, OpenAIApi } from "openai";
+import express from 'express';
+import cors from 'cors';
+import Stripe from 'stripe';
+import MailerLite from '@mailerlite/mailerlite-nodejs';
+
+
+const mailerLite = new MailerLite({
+  api_key: process.env.MAILERLITE_API_KEY
+});
+
+
+const configuration = new Configuration({
+    apiKey: process.env.OPENAI_API_KEY,
+});
+
+const openai = new OpenAIApi(configuration);
+const app = express();
+app.use(cors());
+app.use(express.json());
+
+const getCompletion = async (prompt) => {
+    return await openai.createChatCompletion({
+        model: "gpt-4",
+        messages: prompt,
+        max_tokens: 500
+    });
+};
+
+const stripe = new Stripe(process.env.STRIPE_SECRET_KEY);
+
+app.post('/create-payment-intent', async (req, res) => {
+  const { amount } = req.body;
+  const paymentIntent = await stripe.paymentIntents.create({
+    amount,
+    currency: 'eur',
+    
+  
+  });
+
+  res.json({ client_secret: paymentIntent.client_secret });
+});
+
+const addSubscriber = async (email, name, groupId) => {
+  const params = {
+    email,
+    fields: {
+      name
+    },
+    groups: [groupId],
+    status: "active"
+  };
+
+  try {
+    const response = await mailerLite.subscribers.createOrUpdate(params);
+    console.log(response.data);
+  } catch (error) {
+    if (error.response) {
+      console.log(error.response.data);
+    }
+  }
+};
+
+
+app.post('/subscribe', async (req, res) => {
+  const { email, name, groupId } = req.body;
+
+  try {
+    const subscriber = await addSubscriber(email, name, groupId);
+    res.status(200).send({ message: 'Subscriber added successfully', subscriber });
+  } catch (error) {
+    console.error('Error adding subscriber:', error);
+    res.status(500).send({ error });
+  }
+});
+
+
+app.post('/questions', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const completion = await getCompletion(prompt);
+        res.status(200).send({ message: completion.data.choices[0].message.content });
+    } catch (error) {
+        res.status(500).send({ error });
+    }
+});
+
+app.post('/insight', async (req, res) => {
+    try {
+        const prompt = req.body.prompt;
+        const completion = await getCompletion(prompt);
+        res.status(200).send({ message: completion.data.choices[0].message.content });
+    } catch (error) {
+        res.status(500).send({ error });
+    }
+});
+
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server is running on port ${PORT}`);
+});
+
